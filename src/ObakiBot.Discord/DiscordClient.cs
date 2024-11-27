34ab8488f73885f98104e48247a8aff45c32cf7e
joshuajpiluden@gmail.com <joshuajpiluden@Gmail.com>
2024-11-27 14:09:46 +0800

    Added IsClientStarted property to track client state

--- src/ObakiBot.Discord/DiscordClient.cs
+++ src/ObakiBot.Discord/DiscordClient.cs
@@ -5,61 +5,97 @@ using NetCord.Services.Commands;
 
 namespace ObakiBot.Discord;
 
-  public sealed class DiscordClient : IDisposable
+public sealed class DiscordClient : IDisposable
+{
+    private readonly GatewayClient _gatewayClient;
+    private readonly ILogger<DiscordClient> _logger;
+    private bool _isClientStarted;
+    public DiscordEventContext DiscordEventContext { get; }
+    public bool IsClientStarted => _isClientStarted;
+
+    public DiscordClient(
+        GatewayClient gatewayClient,
+        ApplicationCommandService<SlashCommandContext> slashCommandService,
+        CommandService<CommandContext> commandService,
+        IServiceProvider serviceProvider,
+        ILogger<DiscordClient> logger)
     {
-        private readonly GatewayClient _gatewayClient;
-        private readonly IServiceProvider _serviceProvider;
-        private readonly ILogger<DiscordClient> _logger;
+        _gatewayClient = gatewayClient ?? throw new ArgumentNullException(nameof(gatewayClient));
+        _logger = logger ?? throw new ArgumentNullException(nameof(logger));
 
-        public DiscordClient(
-            GatewayClient gatewayClient,
-            ApplicationCommandService<SlashCommandContext> slashCommandService,
-            CommandService<CommandContext> commandService,
-            IServiceProvider serviceProvider,
-            ILogger<DiscordClient>  logger)
+        DiscordEventContext = CreateEventContext(gatewayClient, slashCommandService, commandService, serviceProvider, logger);
+    }
+
+    public async Task InitializeClientAsync()
+    {
+         _logger.LogInformation("Initializing Discord client...");
+        DiscordEventContext.RegisterEvents();
+        await DiscordEventContext.InitializeCommandsAsync();
+         _logger.LogInformation("Discord client initialized successfully.");
+    }
+
+    public async Task StartClientAsync()
+    {
+        EnsureClientNotNull();
+        if (_isClientStarted)
         {
-            _gatewayClient = gatewayClient;
-            _serviceProvider = serviceProvider;
-            _logger = logger;
-
-            DiscordEventContext = new DiscordEventContext(
-                gatewayClient,
-                slashCommandService,
-                commandService,
-                serviceProvider,
-                logger
-            );
-        }
-        
-        public DiscordEventContext DiscordEventContext { get; }
-
-        public async Task InitializeClientAsync()
-        {
-            _logger.LogInformation("Initializing Discord client...");
-            DiscordEventContext.RegisterEvents();
-            await DiscordEventContext.InitializeCommandsAsync();
-            _logger.LogInformation("Discord client initialized successfully.");
+            _logger.LogInformation("Discord client is already started. Start operation skipped.");
+            return;
         }
 
-        public async Task StartClientAsync()
+        EnsureClientNotNull();
+
+        _logger.LogInformation("Starting Discord client...");
+        await _gatewayClient.StartAsync();
+        _isClientStarted = true;
+        _logger.LogInformation("Discord client started.");
+    }
+
+    public async Task StopClientAsync()
+    {
+        if (!_isClientStarted)
         {
-            ArgumentNullException.ThrowIfNull(_gatewayClient);
-            _logger.LogInformation("Starting Discord client...");
-            await _gatewayClient.StartAsync();
-            _logger.LogInformation("Discord client started.");
+            _logger.LogInformation("Discord client is not running. Stop operation skipped.");
+            return;
         }
 
-        public async Task StopClientAsync()
-        {
-            _logger.LogInformation("Stopping Discord client...");
-            await _gatewayClient.CloseAsync();
-            _logger.LogInformation("Discord client stopped.");
-        }
+        _logger.LogInformation("Stopping Discord client...");
+        await _gatewayClient.CloseAsync();
+        _isClientStarted = false;
+        _logger.LogInformation("Discord client stopped.");
+    }
 
-        public void Dispose()
+ 
+    private DiscordEventContext CreateEventContext(
+        GatewayClient gatewayClient,
+        ApplicationCommandService<SlashCommandContext> slashCommandService,
+        CommandService<CommandContext> commandService,
+        IServiceProvider serviceProvider,
+        ILogger logger)
+    {
+        return new DiscordEventContext(gatewayClient, slashCommandService, commandService, serviceProvider, logger);
+    }
+
+    private void EnsureClientNotNull()
+    {
+        if (_gatewayClient == null)
         {
-            _logger.LogInformation("Disposing Discord client...");
-            _gatewayClient?.Dispose();
-            _logger.LogInformation("Discord client disposed.");
+            throw new InvalidOperationException("GatewayClient is not initialized.");
         }
     }
+    
+    public void Dispose()
+    {
+        if (_isClientStarted)
+        {
+            _logger.LogInformation("Stopping Discord client before disposal...");
+            _gatewayClient.CloseAsync();
+            _isClientStarted = false;
+        }
+
+        _logger.LogInformation("Disposing Discord client...");
+        _gatewayClient?.Dispose();
+        _logger.LogInformation("Discord client disposed.");
+    }
+
+}
---
+++ src/ObakiBot.Ui/Components/Pages/Home.razor.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Components;
+using NetCord.Gateway;
+using ObakiBot.Discord;
+
+namespace ObakiBot.Ui.Components.Pages;
+
+public partial class Home : ComponentBase, IDisposable
+{
+    private readonly DiscordClient _discordClient;
+    private readonly List<string> _logs = [];
+    private bool _isClientstarted;
+
+    public Home(DiscordClient discordClient)
+    {
+        _discordClient = discordClient;
+    }
+
+    protected override async Task OnInitializedAsync()
+    {
+        _isClientstarted = _discordClient.IsClientStarted;
+        _discordClient.DiscordEventContext.OnLogAsync += LogTask;
+        await _discordClient.InitializeClientAsync();
+    }
+
+    private async Task StartClient()
+    {
+        _isClientstarted = true;
+        await _discordClient.StartClientAsync();
+    }
+
+    private async Task StopClient()
+    {
+        _isClientstarted = false;
+        await _discordClient.StopClientAsync();
+    }
+
+    private async ValueTask LogTask(LogMessage logMessage)
+    {
+        var log = logMessage.ToString();
+        _logs.Add(log);
+        await InvokeAsync(StateHasChanged);
+    }
+
+    private async ValueTask OnMessageReceived(Message message)
+    {
+        _logs.Add(message.Content);
+        await InvokeAsync(StateHasChanged);
+    }
+
+    public void Dispose()
+    {
+        _discordClient?.Dispose();
+    }
+}
